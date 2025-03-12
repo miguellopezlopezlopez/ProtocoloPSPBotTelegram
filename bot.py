918270f3ae4f9911bf9ba45e73d7b8d503e29050
Miguel López López <lopezlopezm201@gmail.com>
2025-03-12 13:30:06 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,8 +4,8 @@ from telegram import Update, Message
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
 
 # Configuración del bot de Telegram
-TOKEN = "7287970794:AAHKRKvMqar1GaJWSbHX00QTrT-Zyean8Ko"
-CHAT_ID_GRUPO = "-1002315713607"  # Reemplaza con el ID del chat de Telegram
+TOKEN = ""  #Reemplaza con el token de tu Bot
+CHAT_ID_GRUPO = ""  # Reemplaza con el ID del chat de Telegram
 
 # Configuración del servidor TCP
 SERVER_IP = "127.0.0.1"  # Cambia si el servidor está en otra máquina
